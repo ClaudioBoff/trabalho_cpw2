83ae96ab2d094f2c17a352795cb6c564337c7b24
ClaudioBoff <boff.claudio@gmail.com>
2023-12-01 00:04:09 -0300

    added a player

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import Phaser from "phaser";
 import casa from "./assets/sproutLands/tilesets/Wooden House.png";
 import grama from "./assets/sproutLands/tilesets/Grass.png";
 import moveis from "./assets/sproutLands/objects/basicFurniture.png";
-import mapaCasaJSON from "./assets/casa.json"
+import mapaCasaJSON from "./assets/world_map.json";
+import bioma from "./assets/sproutLands/objects/Basic Grass Biom things 1.png";
+import agua from "./assets/sproutLands/tilesets/Water.png";
+import hills from "./assets/sproutLands/tilesets/Hills.png";
+import playerOne from "./assets/sproutLands/characters/Basic Charakter Spritesheet.png";
 
 
 const config = {
@@ -10,11 +14,13 @@ const config = {
     type: Phaser.AUTO,
     width: 9*16*4,
     height: 9*16*4,
+    zoom: 3,
     mode: Phaser.Scale.CENTER_BOTH,
     physics: {
       default: "arcade",
       arcade: {
         gravity: { y: 0 },
+        debug: true
       },
     },
     scene: {
@@ -33,36 +39,79 @@ const config = {
 
   function preload() {
     this.load.image("grama", grama);
-    this.load.image("tiles", casa);
+    this.load.image("agua", agua);
+    this.load.image("casa", casa);
     this.load.image("moveis", moveis);
     this.load.tilemapTiledJSON("map", mapaCasaJSON);
+    this.load.image("bioma", bioma);
+    this.load.image("water", agua);
+    this.load.image("hills", hills);
+    this.load.spritesheet('playerOne', playerOne, { frameWidth: 48, frameHeight: 48 });
   }
   
   function create() {
     const map = this.make.tilemap({ key: "map" })
-    const casaTileset = map.addTilesetImage("Wooden House", "tiles");
-    const gramaTileset = map.addTilesetImage("Grass", "grama");
+    const casaTileset = map.addTilesetImage("Wooden House", "casa");
+    const gramaTileset = map.addTilesetImage("Grama", "grama");
+    const biomaTileset = map.addTilesetImage("Bioma", "bioma");
+    const aguaTileset = map.addTilesetImage("Water", "agua");
+    const hillsTileset = map.addTilesetImage("Hills", "hills");
+    const moveisTileset = map.addTilesetImage("basicFurniture", "moveis");
 
-    const fundoCasaLayer = map.createLayer("Fundo_casa", casaTileset, 0, 0).setScale(4);
-    const fundoGramaLayer = map.createLayer("Fundo_grama", gramaTileset, 0, 0).setScale(4);
-    const ParedesLayer = map.createLayer("Paredes", casaTileset,0,0).setScale(4);
 
+    const fundoAguaLayer = map.createLayer("Fundo_agua", aguaTileset, 0, 0);//.setScale(4);
+    const fundoGramaLayer = map.createLayer("Fundo_grama", gramaTileset, 0, 0);//.setScale(4);
+    const altosLayer = map.createLayer("Altos", hillsTileset, 0, 0);//.setScale(4);
+    const fundoCasaLayer = map.createLayer("Fundo_casa", casaTileset, 0, 0);//.setScale(4);
+    const casaLayer = map.createLayer("Casa", casaTileset, 0, 0);//.setScale(4);
+    const moveisCasaLayer = map.createLayer("Moveis_casa", moveisTileset, 0, 0);//.setScale(4);
+    const biomaLayer = map.createLayer("Bioma", biomaTileset, 0, 0);//.setScale(4);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    const spawnPointObject = map.findObject("Player_spawn", (obj) => obj.name === "spawnPoint");
+
+    if (spawnPointObject) {
+      // Retrieve spawn point coordinates
+      const spawnX = spawnPointObject.x;
+      const spawnY = spawnPointObject.y;
+
+      // Spawn the player at the specified coordinates
+      player = this.physics.add.sprite(spawnX, spawnY, 'playerOne');
+      this.cameras.main.startFollow(player);
+      this.physics.world.enable(player);
+    }
+       this.anims.create({
+        key: 'player_walk', // Animation key
+        frames: this.anims.generateFrameNumbers('playerOne', { start: 0, end: 3 }), // Adjust frame range
+        frameRate: 6, // Adjust frame rate
+        repeat: -1, // -1 means loop indefinitely
+    }) ;
+    player.anims.play('player_walk', true);
+    
   }
-//    const moveisTileset = map.addTilesetImage("moveis", "moveis");
-  
-   /* map.createStaticLayer("Fundo",tileset);
-    map.createStaticLayer("Paredes",tileset);
-  /* 
-    const Fundo = map.createStaticLayer("Fundo", tileset, 0, 0);
-    const Casa = map.createStaticLayer("Casa", [tileset, moveisTileset], 0, 0);
-    const Atravessavel = map.createStaticLayer("Atravessavel", tileset, 0, 0);
-    const Colisor = map.createStaticLayer("Colisor", tileset, 0, 0);
-  
-    Colisor.setCollisionByProperty({ collider: true });
-    Casa.setCollisionByProperty({ collider: true });
-    Atravessavel.setDepth(10);*/
-  //}
+
 
   function update() {
+    console.log("Player X:", player.x, "Player Y:", player.y);
+    player.body.offset.x =  16;
+    player.body.offset.y = 16;
+    player.body.width = 16;
+    player.body.height = 16;
 
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160); // Adjust the velocity based on your needs
+  } else if (cursors.right.isDown) {
+      player.setVelocityX(160); // Adjust the velocity based on your needs
+  } else {
+      player.setVelocityX(0);
+  }
+
+  if (cursors.up.isDown) {
+      player.setVelocityY(-160); // Adjust the velocity based on your needs
+  } else if (cursors.down.isDown) {
+      player.setVelocityY(160); // Adjust the velocity based on your needs
+  } else {
+      player.setVelocityY(0);
+  }
   }
